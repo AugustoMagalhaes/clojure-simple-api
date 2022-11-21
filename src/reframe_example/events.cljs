38fa0439abf1577fecf83f3cb910b06f4bb7a1cc
AugustoMagalhaes <augustommg@gmail.com>
2022-11-21 17:52:54 -0300

    chore: altera tpo de ::initialize-db fn-traced -> fn

--- src/reframe_example/events.cljs
+++ src/reframe_example/events.cljs
@@ -1,13 +1,12 @@
 (ns reframe-example.events
   (:require [ajax.core :as ajax]
             [day8.re-frame.http-fx]
-            [day8.re-frame.tracing :refer-macros [fn-traced]]
             [re-frame.core :as rf]
             [reframe-example.db :as db]))
 
 (rf/reg-event-db
  ::initialize-db
- (fn-traced [_ _]
+ (fn [_ _]
    db/default-db))
 
 (rf/reg-event-db
